7d55c28823f08dc6b65da0668db9531dcf7e9e81
jebitok-dev <sharonjebitok@gmail.com>
2021-08-18 18:12:49 +0300

    Add Heroku Config

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const sendMail = require('./mail');
 
 app.set('view engine', 'ejs');
 
@@ -15,6 +17,31 @@ app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
+app.use(express.urlencoded({
+    extend: false
+}));
+
+app.use(express.json());
+
 app.listen(8080);
+
+app.post('/', (req, res) => {
+    //Send an email here but currently dummy email
+    const { name, subject, email, text } = req.body;
+    console.log('Data:', req.body);
+    // res.json({message: 'Message received!'})
+    sendMail(name, email, subject, text, function(err, data) {
+        if (err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            res.status({ message: 'Email sent!!!' });
+        }
+    });
+});
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'pages', 'index.ejs'));
+    //__dirname : It will resolve to your project folder.
+});
 
 console.log('Server is listening on port 8080');
